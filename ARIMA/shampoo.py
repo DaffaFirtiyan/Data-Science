abd0f5844391bcbee760357f1cde806aeebe9e51
Daffa Firtiyan <mdaffa.anzalna@gmail.com>
2023-02-08 19:08:20 +1300

    updated readme

--- ARIMA/shampoo.py
+++ ARIMA/shampoo.py
@@ -1,8 +1,10 @@
 from pandas import read_csv
 from datetime import datetime
 from matplotlib import pyplot
-from pandas.plotting import autocorrelation_plot
+# from pandas.plotting import autocorrelation_plot
 from pandas import DataFrame
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 from statsmodels.tsa.arima.model import ARIMA
 
 # load dataset
@@ -13,24 +15,52 @@ series = read_csv("ARIMA/shampoo.csv", header=0, parse_dates=[0], index_col=0, d
 
 # convert index to datetime
 series.index = series.index.to_period("M")
+
+# split into train and test sets
+X = series.values
+size = int(len(X)*0.66)
+train, test = X[0:size], X[size:len(X)]
+history = [x for x in train]
+predictions = list()
+
+# walk forward validatoin
+for t in range(len(test)):
+    model = ARIMA(history, order=(5,1,0))
+    model_fit = model.fit()
+    output = model_fit.forecast()
+    yHat = output[0]
+    predictions.append(yHat)
+    obs = test[t]
+    history.append(obs)
+    print('predicted=%f, expected=%f' % (yHat, obs))
+
+# evaluate forecasts
+rmse = sqrt(mean_squared_error(test, predictions))
+print('Test RMSE: %.3f' % rmse)
+
+# plot forecasts against actual outcomes
+pyplot.plot(test)
+pyplot.plot(predictions, color='red')
+pyplot.show()
+
 # autocorrelation_plot(series)
 # pyplot.show()
 
-# fit model
-model = ARIMA(series, order=(5,1,0))
-model_fit = model.fit()
+# # fit model
+# model = ARIMA(series, order=(5,1,0))
+# model_fit = model.fit()
 
-#summary of the fit model
-print(model_fit.summary())
+# #summary of the fit model
+# print(model_fit.summary())
 
-# line plot of residuals
-residuals = DataFrame(model_fit.resid)
-residuals.plot()
-pyplot.show()
+# # line plot of residuals
+# residuals = DataFrame(model_fit.resid)
+# residuals.plot()
+# pyplot.show()
 
-# density plot of residuals
-residuals.plot(kind='kde')
-pyplot.show()
+# # density plot of residuals
+# residuals.plot(kind='kde')
+# pyplot.show()
 
-# summary stats of residuals
-print(residuals.describe())
+# # summary stats of residuals
+# print(residuals.describe())
--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 shampoo.py
 I attempt to apply the ARIMA model to this time series data of monthly shampoo sales over a 3 year period.
 
+TLDR: This code uses the ARIMA model to predict future values of a given dataset. It first loads the dataset, splits it into a train and test set, and then uses the ARIMA model to make predictions on the test set. It then evaluates the forecasts using root mean squared error (RMSE) and plots the results against the actual outcomes.
+
 After parsing through the data, we can see a clear trend. This suggests t aht time series is not stationary and will required differencing to make it stationary, at least a difference order of 1.
 
 After taking a quick look at an autocorrelation plot of the time seires, we can see that the plot shows a positive correlation with the first 10-12 lags, the correlation is particularlong strong for the first 5 lags. This means that we 5 for "p" (autoregression) might be a good starting point.
@@ -37,4 +39,10 @@ The line plot of the residuals errors whos that the current model may not have c
 
 From the density plot of residuals we see that they have a Gaussian distribution but doesn't have a mean of 0, or are not centered around zero. This shows that the residual errors are biassed, meaning that on average, the eerrors are either over or under estimating the actual values.
 
-Now we perform this analysis on the training dataset.
+Now we use the ARIMA model to forecast future time steps.
+
+We print the prediction and the actual value for each iteration and the final RMSE is calculated to compare the accuracy of the model's predictions which is 89.021.
+
+A line plot is created to visualise the expected values (in blue) vs. the rolling forecast predictions (in red), showing that the model captures some trends and is in the correct scale. 
+
+There is potentitial for furether improvement by adjusting the p, d, q param of the model.
